6646bd8692f6bbb55f9ea69c564936c4f040a4c0
Nick Buzzerio <nick.buzzerio@hotmail.com>
2025-02-09 22:26:48 -0500

    setup lasers and firing

---
+++ Assets/Scripts/PlayerWeapon.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerWeapon : MonoBehaviour
+{
+    [SerializeField] GameObject[] lasers;
+    [SerializeField] RectTransform crosshair;
+    [SerializeField] Transform targetPoint;
+    [SerializeField] float targetDistance = 250f;
+    bool isFiring = false;
+
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        ProcessFiring();
+        MoveCrosshair();
+        MoveTargetPoint();
+        AimLasers();
+    }
+
+    public void OnFire(InputValue value)
+    {
+        isFiring = value.isPressed;
+    }
+
+    void ProcessFiring()
+    {
+
+        foreach (GameObject laser in lasers)
+        {
+            ParticleSystem.EmissionModule emissionModule = laser.GetComponent<ParticleSystem>().emission;
+
+            emissionModule.enabled = isFiring;
+        }
+    }
+
+    void MoveCrosshair()
+    {
+        crosshair.position = Input.mousePosition;
+    }
+    void MoveTargetPoint()
+    {
+        Vector3 targetPointPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, targetDistance);
+        targetPoint.position = Camera.main.ScreenToWorldPoint(targetPointPosition);
+    }
+
+    void AimLasers() {
+        foreach (GameObject laser in lasers)
+        {
+            Vector3 fireDirection = targetPoint.position - transform.position;
+            Quaternion rotationToTarget = Quaternion.LookRotation(fireDirection);
+            laser.transform.rotation = rotationToTarget;
+        }
+    }
+}
