e7b77edd8c4630bee60cc88d72f570518e68f035
nbuzzerio <nick.buzzerio@hotmail.com>
2025-02-28 02:13:18 -0500

    added scoreboard, shooting, and particle collision

--- Assets/Scripts/ControllCollision.cs
+++ Assets/Scripts/ControllCollision.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class ControllCollision : MonoBehaviour
 {
+    [SerializeField] GameObject destoyedVFX;
     void OnTriggerEnter(Collider other)
     {
         Debug.Log(other.gameObject.name);
+        Instantiate(destoyedVFX, transform.position, destoyedVFX.transform.rotation);
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,9 +3,29 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject destoyedVFX;
+    [SerializeField] int hitPoints = 6;
+    [SerializeField] int scoreValue = 10;
+
+    Scoreboard scoreboard;
+
+    private void Start()
+    {
+        scoreboard = FindAnyObjectByType<Scoreboard>();
+    }
     private void OnParticleCollision(GameObject other)
     {
-        Instantiate(destoyedVFX, transform.position, destoyedVFX.transform.rotation); //Quarternion.identity for spawn 000 rotation instead of the last arugment which is the parent objects rotation
-        Destroy(this.gameObject);
+        ProcessHit();
+    }
+
+    private void ProcessHit()
+    {
+        hitPoints--;
+
+        if (hitPoints <= 0)
+        {
+            scoreboard.IncreaseScore(scoreValue);
+            Instantiate(destoyedVFX, transform.position, destoyedVFX.transform.rotation); //Quarternion.identity for spawn 000 rotation instead of the last arugment which is the parent objects rotation
+            Destroy(this.gameObject);
+        }
     }
 }
